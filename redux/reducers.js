c741c17cdd5d60d1011bb6e72b23a65b1d2da01d
Davide57 <davide5713@gmail.com>
2018-04-13 01:59:36 +0200

    Combined new report reducer with old match reducer

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,13 +1,12 @@
 
 import {combineReducers} from "redux";
 import persist from './persist';
-import HomeReducer from '../pages/Home/homeReducers';
+import Report from '../pages/Report/reportReducer';
 import Match from '../pages/Match/matchReducer';
-import Levels from '../pages/Levels/levelsReducer';
 
 
 export default combineReducers({
     persist,
-    Levels,
-    Match
+    Match,
+    Report
 });
